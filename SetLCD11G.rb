f8579f3a2fc93930848f596f6695e8b03ada3111
Richard Hesse <richard@mochimedia.com>
2011-05-12 12:10:21 -0700

    initial commit

---
+++ SetLCD11G.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+host = `hostname`.chomp
+
+if host.index('.')
+  hostname = host.split(".")[0].slice(0..13)
+else
+  hostname = host.slice(0..13)
+end
+host_hex = hostname.unpack("c*").map { |e|"0x#{e.to_s(16)}" }
+system "/usr/bin/ipmitool raw 0x6 0x58 0xC2 0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0"
+system "/usr/bin/ipmitool raw 0x6 0x58 0xC1 0 0 #{host_hex.length} #{host_hex.join(' ')}"
